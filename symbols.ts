e9b24de1fbf98a71ee87c6aa9119adb06e923216
nao20010128nao <nao20010128@gmail.com>
2015-08-22 11:23:02 +0900

    シンボルファイル一応完成

--- symbols.ts
+++ symbols.ts
@@ -8,17 +8,59 @@ var java = {
             newInstance: function () {
 
             },
-            getMethod: function (name,types) {
+            getMethod: function (name, types) {
 
             },
             getConstructor: function (types) { }
         },
         ProcessBuilder: function () {
 
+        },
+        reflect: {
+            Array: {
+                newInstance: function (typeObj: any, length: number): any {
+
+                }
+            }
         }
+    },
+    io: {
+        File: function (path: any, addition: String) {
+
+        },
+        FileInputStream: function (path: any) {
+
+        },
+        FileReader: function (path: any) {
+
+        },
+        FileOutputStream: function (path: any) {
+
+        },
+        FileWriter: function (path: any) {
+
+        },
     }
-}, com, android;
-java.lang.ProcessBuilder.prototype.command = function (args) {
-    return this;
-}
+},
+    com = {
+        minecraftpe: {
+            MainActivity: {
+                currentMainActivity: {
+                    get: function () { }
+                }
+            }
+        },
+    },
+    android = {
+        app: {
+            AlertDialog: function (context: any) {
+
+            }
+        },
+        media: {
+            MediaPlayer: function () {
+
+            }
+        }
+    };
 /*End Virtual Symbols*/
